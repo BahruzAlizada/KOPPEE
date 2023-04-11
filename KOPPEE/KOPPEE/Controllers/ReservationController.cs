89872b554f9d779d339f30904601ffab9e64456f
BahruzAlizada <behruzelzadee@gmail.com>
2023-04-11 16:33:26 +0400

    Front to Back

---
+++ KOPPEE/KOPPEE/Controllers/ReservationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KOPPEE.Controllers
+{
+	public class ReservationController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
